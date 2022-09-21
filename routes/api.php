3ab09bc0f856efa1ee8ce7599d6c10dbb69cbdab
Dev Master <heroldtamko39@gmail.com>
2022-09-21 19:14:16 +0100

    initialize project

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+// use App\Http\Controllers\API\MusicController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::apiResource("music", MusicController::class);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
